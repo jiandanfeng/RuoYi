4a7ea553a76c477829158ed5c0aac3aaf93409c5
chenjh3 <chenjh3@wangsu.com>
2019-10-30 16:19:59 +0800

    启动完成--master

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -23,6 +23,7 @@ public class RuoYiApplication {
     public static void main(String[] args) {
         Instant inst1 = Instant.now();
         SpringApplication.run(RuoYiApplication.class, args);
+        System.out.println("启动完成.....master");
         System.out.println("启动完成.....");
         log.info(":: 若依管理系统Java开发平台  :: 基于 Spring Boot {} ::", SpringBootVersion.getVersion());
         log.info(":: 启动成功!耗时:{}秒 ::", Duration.between(inst1, Instant.now()).getSeconds());
